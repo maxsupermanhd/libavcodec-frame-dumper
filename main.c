b0699ceb1568a09dec48ddcd7602ff3961b682b5
maxsupermanhd <q3.max.2011@ya.ru>
2024-07-24 20:38:55 +0300

    png cleanups

--- main.c
+++ main.c
@@ -27,8 +27,8 @@ typedef struct {
 static int save_png_to_file(bitmap_t *bitmap, const char *path);
 
 int main(int argc, char **argv) {
-	if (argc < 4) {
-		logging("usage %s videoPath videoBeginTime outPath", argv[0]);
+	if (argc < 5) {
+		logging("usage %s videoPath videoBeginTime outPath seekToFrame", argv[0]);
 		return 1;
 	}
 	videoBeginTime = atoi(argv[2]);
@@ -268,51 +268,41 @@ static void getRGBfromFrame(AVFrame* frame, int* or, int* og, int* ob, int x, in
 	*ob = round(b);
 }
 
-static pixel_t * pixel_at (bitmap_t * bitmap, int x, int y) {
+static pixel_t * pixel_at(bitmap_t * bitmap, int x, int y) {
 	return bitmap->pixels + bitmap->width * y + x;
 }
 
 static int save_png_to_file(bitmap_t *bitmap, const char *path) {
-	FILE * fp;
 	png_structp png_ptr = NULL;
 	png_infop info_ptr = NULL;
-	size_t x, y;
 	png_byte ** row_pointers = NULL;
-	/* "status" contains the return value of this function. At first
-	it is set to a value which means 'failure'. When the routine
-	has finished its work, it is set to a value which means
-	'success'. */
-	int status = -1;
-	/* The following number is set by trial and error only. I cannot
-	see where it it is documented in the libpng manual.
-	*/
 	int pixel_size = 3;
 	int depth = 8;
 	
-	fp = fopen (path, "wb");
-	if (! fp) {
-		goto fopen_failed;
+	FILE* fp = fopen(path, "wb");
+	if (!fp) {
+		return -1;
 	}
 
 	png_ptr = png_create_write_struct (PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 	if (png_ptr == NULL) {
-		goto png_create_write_struct_failed;
+		fclose(fp);
+		return -1;
 	}
 	
 	info_ptr = png_create_info_struct (png_ptr);
 	if (info_ptr == NULL) {
-		goto png_create_info_struct_failed;
+		fclose(fp);
+		return -1;
 	}
-	
-	/* Set up error handling. */
 
-	if (setjmp (png_jmpbuf (png_ptr))) {
-		goto png_failure;
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		png_destroy_write_struct(&png_ptr, &info_ptr);
+		fclose(fp);
+		return -1;
 	}
-	
-	/* Set image attributes. */
 
-	png_set_IHDR (png_ptr,
+	png_set_IHDR(png_ptr,
 				info_ptr,
 				bitmap->width,
 				bitmap->height,
@@ -321,45 +311,29 @@ static int save_png_to_file(bitmap_t *bitmap, const char *path) {
 				PNG_INTERLACE_NONE,
 				PNG_COMPRESSION_TYPE_DEFAULT,
 				PNG_FILTER_TYPE_DEFAULT);
-	
-	/* Initialize rows of PNG. */
 
-	row_pointers = png_malloc (png_ptr, bitmap->height * sizeof (png_byte *));
-	for (y = 0; y < bitmap->height; y++) {
-		png_byte *row = 
-			png_malloc (png_ptr, sizeof (uint8_t) * bitmap->width * pixel_size);
+	row_pointers = png_malloc(png_ptr, bitmap->height * sizeof (png_byte *));
+	for (size_t y = 0; y < bitmap->height; y++) {
+		png_byte *row = png_malloc(png_ptr, sizeof (uint8_t) * bitmap->width * pixel_size);
 		row_pointers[y] = row;
-		for (x = 0; x < bitmap->width; x++) {
-			pixel_t * pixel = pixel_at (bitmap, x, y);
+		for (size_t x = 0; x < bitmap->width; x++) {
+			pixel_t * pixel = pixel_at(bitmap, x, y);
 			*row++ = pixel->red;
 			*row++ = pixel->green;
 			*row++ = pixel->blue;
 		}
 	}
-	
-	/* Write the image data to "fp". */
-
-	png_init_io (png_ptr, fp);
-	png_set_rows (png_ptr, info_ptr, row_pointers);
-	png_write_png (png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
-
-	/* The routine has successfully written the file, so we set
-	"status" to a value which indicates success. */
-
-	status = 0;
-	
-	for (y = 0; y < bitmap->height; y++) {
-		png_free (png_ptr, row_pointers[y]);
+	png_init_io(png_ptr, fp);
+	png_set_rows(png_ptr, info_ptr, row_pointers);
+	png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+	for (size_t y = 0; y < bitmap->height; y++) {
+		png_free(png_ptr, row_pointers[y]);
 	}
-	png_free (png_ptr, row_pointers);
+	png_free(png_ptr, row_pointers);
 	
-png_failure:
-png_create_info_struct_failed:
-	png_destroy_write_struct (&png_ptr, &info_ptr);
-png_create_write_struct_failed:
+	png_destroy_write_struct(&png_ptr, &info_ptr);
 	fclose (fp);
-fopen_failed:
-	return status;
+	return 0;
 }
 
 static void logging(const char *fmt, ...) {
